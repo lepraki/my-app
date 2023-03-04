551a0b14759de69a2d1ea7fa07150763f9fbd7e6
Daniel Maya <lepraki@hotmail.com>
2023-03-04 00:21:57 -0500

    challenge4

---
+++ src/GifExpert.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpert = () => {
+
+	const [categories, setCategories] = useState([''])
+	
+	const onAddCategory = (category) => {
+		setCategories((list) => [...list, category]);
+	};
+
+	return (
+		<>
+			<h1>GifExpert</h1>
+
+			<AddCategory onAddCategory={onAddCategory} />
+			{
+				categories.map(
+					(category, key)=>{
+						return <GifGrid category={category} key={key} />
+					}
+				)
+			}
+			
+		</>
+	);
+};
